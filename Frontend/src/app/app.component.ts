a05d7e8d0153c569907cbfee86ecc16ca998d054
mahfuzullahmufi <mahfuzullah1999@gmail.com>
2024-08-09 01:54:35 +0600

    Setting up frontend layout.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,24 +1,50 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NbSidebarService } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  
-  title = 'Information-Collector';
-  infos: any[];
+export class AppComponent {
 
-  constructor (private http: HttpClient) { }
+  constructor(private sidebarService: NbSidebarService) {}
+
+  toggleSidebar = (): void => {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+  }
+
+  items = [
+    { title: 'Profile' },
+    { title: 'Logout' },
+  ];
+
+  menuItems = [
+    {
+      title: 'Dashboard',
+      link: '/',
+      icon: 'home-outline',
+    },
+    {
+      title: 'Person Information',
+      icon: 'person-outline',
+      expanded: false,
+      children: [
+        {
+          title: 'Collect Information',
+          link: '/information/collect-information',
+          icon: 'edit-outline',
+        },
+        {
+          title: 'View Information',
+          link: '/information/view-information',
+          icon: 'eye-outline',
+        },
+      ],
+    },
+  ];
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7258/api/Information').subscribe((response : any) => {
-      this.infos = response;
-    }, error => {
-      console.log(error);
-    });
-    
+    // Initialization logic here
   }
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,31 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetInfoComponent } from './excercise/get-info/get-info.component';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CountryComponent } from './excercise/country/country.component';
 import { EditCountryComponent } from './excercise/edit-country/edit-country.component';
-import { NbThemeModule, NbLayoutModule, NbDatepickerModule, NbToastrModule, NbMenuModule } from '@nebular/theme';
+import { ExamplePdfViewerComponent } from './excercise/example-pdf-viewer/example-pdf-viewer.component';
+import { NbThemeModule, NbLayoutModule, NbDatepickerModule, NbToastrModule, NbMenuModule, NbSidebarModule, NbActionsModule, NbUserModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { DataTablesModule } from 'angular-datatables';
-import { TopNavbarComponent } from './dashboard/top-navbar/top-navbar.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { ExamplePdfViewerComponent } from './excercise/example-pdf-viewer/example-pdf-viewer.component';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
-@NgModule({ declarations: [
+@NgModule({
+    declarations: [
         AppComponent,
         GetInfoComponent,
         CountryComponent,
         EditCountryComponent,
         ExamplePdfViewerComponent,
     ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         FormsModule,
@@ -38,5 +38,14 @@ import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
         DataTablesModule,
         DashboardModule,
         NbMenuModule.forRoot(),
-        NgxExtendedPdfViewerModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        NbSidebarModule.forRoot(),
+        NgxExtendedPdfViewerModule,
+        NbActionsModule,
+        NbActionsModule,
+        NbUserModule
+    ],
+    providers: [
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
 export class AppModule { }
